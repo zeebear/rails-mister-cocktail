f0bcadf19511015cf18ac177228fbe6c12f3fcc6
Claire Fahy <zeebear21@gmail.com>
2019-02-15 13:26:16 -0300

    Add dotenv, background image, _card.scss

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -13,9 +13,9 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = Cocktail.find(@dose.cocktail_id)
+    # @cocktail = Cocktail.find(@dose.cocktail_id)
     @dose.destroy
-    redirect_to @cocktail
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
